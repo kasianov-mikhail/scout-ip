d2a58873a6f412c0bda7511e640de6fe89d42698
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-09-08 23:23:42 +0300

    Refactor AppDelegate and SceneDelegate into separate files
    Moved AppDelegate and SceneDelegate classes from ScoutIPApp.swift into their own files (AppDelegate.swift and SceneDelegate.swift) for better code organization and separation of concerns. Updated ScoutIPApp.swift to remove the inlined classes and related imports.

---
+++ ScoutIP/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+// Copyright 2025 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import Scout
+import UIKit
+import CloudKit
+
+let container = CKContainer(identifier: "iCloud.Logging.Scout.0001")
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        do {
+            try Scout.setup(container: container)
+        } catch {
+            logger.error("Scout setup error: \(error.localizedDescription)")
+        }
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let configuration = UISceneConfiguration(
+            name: connectingSceneSession.configuration.name,
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
+    }
+}
