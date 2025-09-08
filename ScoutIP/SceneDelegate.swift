6e14dc02b6f7442f2b6915df3d8f1a7f22cf8c6c
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-09-08 23:23:42 +0300

    Refactor AppDelegate and SceneDelegate into separate files
    Moved AppDelegate and SceneDelegate classes from ScoutIPApp.swift into their own files (AppDelegate.swift and SceneDelegate.swift) for better code organization and separation of concerns. Updated ScoutIPApp.swift to remove the inlined classes and related imports.

---
+++ ScoutIP/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+// Copyright 2025 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import UIKit
+
+var shortcut: String?
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        shortcut = connectionOptions.shortcutItem?.type
+    }
+
+    func windowScene(
+        _ windowScene: UIWindowScene,
+        performActionFor shortcutItem: UIApplicationShortcutItem,
+        completionHandler: @escaping (Bool) -> Void
+    ) {
+        shortcut = shortcutItem.type
+        completionHandler(true)
+    }
+}
--- ScoutIP/ScoutIPApp.swift
+++ ScoutIP/ScoutIPApp.swift
@@ -5,16 +5,11 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import CloudKit
 import Logging
-import Scout
 import SwiftUI
 import UIKit
 
-let container = CKContainer(identifier: "iCloud.Logging.Scout.0001")
 let logger = Logger(label: "com.kasianov.ScoutIP")
-
-var shortcut: String?
 
 @main struct ScoutIPApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
@@ -26,51 +21,5 @@ var shortcut: String?
                 PersistenceController.shared.container.viewContext
             )
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
-        do {
-            try Scout.setup(container: container)
-        } catch {
-            logger.error("Scout setup error: \(error.localizedDescription)")
-        }
-        return true
-    }
-
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(
-            name: connectingSceneSession.configuration.name,
-            sessionRole: connectingSceneSession.role
-        )
-        configuration.delegateClass = SceneDelegate.self
-        return configuration
-    }
-}
-
-class SceneDelegate: NSObject, UIWindowSceneDelegate {
-
-    func scene(
-        _ scene: UIScene, willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        shortcut = connectionOptions.shortcutItem?.type
-    }
-
-    func windowScene(
-        _ windowScene: UIWindowScene, performActionFor shortcutItem: UIApplicationShortcutItem,
-        completionHandler: @escaping (Bool) -> Void
-    ) {
-        shortcut = shortcutItem.type
-        completionHandler(true)
     }
 }
