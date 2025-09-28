c3a5fb08e8040fb059e018a8421c1fbf313a9b15
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2025-09-28 15:40:54 +0300

    Refactor AppDelegate method signatures formatting
    Condensed multi-line method signatures in AppDelegate.swift to single lines for improved readability and consistency.

--- ScoutIP/AppDelegate.swift
+++ ScoutIP/AppDelegate.swift
@@ -12,10 +12,7 @@ import CloudKit
 let container = CKContainer(identifier: "iCloud.Logging.Scout.0001")
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
-    ) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         do {
             try Scout.setup(container: container)
         } catch {
@@ -24,11 +21,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         return true
     }
 
-    func application(
-        _ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions
-    ) -> UISceneConfiguration {
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         let configuration = UISceneConfiguration(
             name: connectingSceneSession.configuration.name,
             sessionRole: connectingSceneSession.role
