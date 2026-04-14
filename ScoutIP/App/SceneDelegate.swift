ff2a44f45c9a3ec081668e4211755bfee4518057
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-04-14 23:47:22 +0300

    Fix copyright year to 2026

--- ScoutIP/App/SceneDelegate.swift
+++ ScoutIP/App/SceneDelegate.swift
@@ -1,5 +1,5 @@
 //
-// Copyright 2025 Mikhail Kasianov
+// Copyright 2026 Mikhail Kasianov
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file or at
