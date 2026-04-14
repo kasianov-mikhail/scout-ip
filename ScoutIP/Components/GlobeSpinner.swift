25a819a9a526f0104c7f462320ee1c84159a70c4
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-04-15 01:16:11 +0300

    Fix iOS 16 compatibility for globe animation

---
+++ ScoutIP/Components/GlobeSpinner.swift
@@ -0,0 +1,23 @@
+//
+// Copyright 2026 Mikhail Kasianov
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import SwiftUI
+
+struct GlobeSpinner: View {
+  @State private var rotation = 0.0
+
+  var body: some View {
+    Image(systemName: "globe")
+      .foregroundStyle(.blue)
+      .rotationEffect(.degrees(rotation))
+      .onAppear {
+        withAnimation(.linear(duration: 1).repeatForever(autoreverses: false)) {
+          rotation = 360
+        }
+      }
+  }
+}
