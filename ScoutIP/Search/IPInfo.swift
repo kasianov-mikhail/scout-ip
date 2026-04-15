18bda0e43d87398861cf84437bc2b9171a65c01b
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-04-15 15:29:17 +0300

    Move mock to IPInfo level to bypass token check

--- ScoutIP/Search/IPInfo.swift
+++ ScoutIP/Search/IPInfo.swift
@@ -23,20 +23,26 @@ class IPInfo: ObservableObject {
     let tracker = IPRecordTracker(source: ip.isEmpty ? .user : .manual)
 
     do {
-      let token: String
-      if let keychainToken = KeychainHelper.load(key: "IPINFO_KEY") {
-          token = keychainToken
-      } else if let plistToken = Bundle.main.infoDictionary?["IPINFO_KEY"] as? String, !plistToken.isEmpty {
-          KeychainHelper.save(key: "IPINFO_KEY", value: plistToken)
-          token = plistToken
+      let object: IPObject
+
+      if MockProvider.isMocking {
+        object = IPObject.mock(ip: ip.isEmpty ? MockProvider.mockIP : ip, context: context)
       } else {
-          return
+        let token: String
+        if let keychainToken = KeychainHelper.load(key: "IPINFO_KEY") {
+            token = keychainToken
+        } else if let plistToken = Bundle.main.infoDictionary?["IPINFO_KEY"] as? String, !plistToken.isEmpty {
+            KeychainHelper.save(key: "IPINFO_KEY", value: plistToken)
+            token = plistToken
+        } else {
+            return
+        }
+
+        tracker.requested()
+
+        let provider = InfoProvider(token: token, ip: ip, context: context)
+        object = try await provider.ipObject()
       }
-
-      tracker.requested()
-
-      let provider = InfoProvider(token: token, ip: ip, context: context)
-      let object = try await provider.ipObject()
 
       let record = IPRecord(context: context)
       record.date = Date()
