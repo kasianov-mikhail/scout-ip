9be5af4648d83ec36f9f08e8577a87bdc604bcb2
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-04-15 16:27:30 +0300

    Revert unnecessary mock arguments in SnapshotTests

--- ScoutIPUITests/SnapshotTests.swift
+++ ScoutIPUITests/SnapshotTests.swift
@@ -13,7 +13,6 @@ final class SnapshotTests: XCTestCase {
 
   override func setUpWithError() throws {
     continueAfterFailure = false
-    app.launchArguments += ["-UITestMocking"]
     app.launch()
   }
 
@@ -40,7 +39,7 @@ final class SnapshotTests: XCTestCase {
   @MainActor
   func testInfoScreenDarkModeSnapshot() {
     app.terminate()
-    app.launchArguments += ["-UITestMocking", "-UIUserInterfaceStyle", "Dark"]
+    app.launchArguments += ["-UIUserInterfaceStyle", "Dark"]
     app.launch()
 
     let screenshot = app.screenshot()
@@ -53,7 +52,7 @@ final class SnapshotTests: XCTestCase {
   @MainActor
   func testHistoryScreenDarkModeSnapshot() {
     app.terminate()
-    app.launchArguments += ["-UITestMocking", "-UIUserInterfaceStyle", "Dark"]
+    app.launchArguments += ["-UIUserInterfaceStyle", "Dark"]
     app.launch()
 
     app.tabBars.buttons["History"].tap()
