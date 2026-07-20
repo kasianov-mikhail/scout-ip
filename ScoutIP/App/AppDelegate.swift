b2fedd3be5d15267e35573506e9d3448ff1cee37
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-07-20 23:01:50 +0300

    Order the LookupIndex import alphabetically

--- ScoutIP/App/AppDelegate.swift
+++ ScoutIP/App/AppDelegate.swift
@@ -5,8 +5,8 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import LookupIndex
 import CloudKit
+import LookupIndex
 import NativeConnector
 import Scout
 import ScoutUI
