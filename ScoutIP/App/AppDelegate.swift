d46b94f4bc35e98cc926f196ded4ecfaa0832685
Mikhail Kasianov <96941969+kasianov-mikhail@users.noreply.github.com>
2026-08-12 19:49:47 +0300

    Switch CloudKit container to iCloud.Logging.Scout.0012

--- ScoutIP/App/AppDelegate.swift
+++ ScoutIP/App/AppDelegate.swift
@@ -21,7 +21,7 @@ import UIKit
 /// served from.
 ///
 var backends: [Backend] {
-    let container = CKContainer(identifier: "iCloud.Logging.Scout.0009")
+    let container = CKContainer(identifier: "iCloud.Logging.Scout.0012")
 
     guard let cloudKit = try? Backend.cloudKit(container: container) else {
         return []
